513e278507f1bf5ab51643d8147d95236a0ad27d
Mohamed Ali <dev.mohali23@gmail.com>
2023-12-02 00:40:32 +0200

    update

--- #1_First_month/Final_Project/Tic_Tac_Toe__Game.py
+++ #1_First_month/Final_Project/Tic_Tac_Toe__Game.py
@@ -1,17 +1,22 @@
 import tkinter as tk
+import random
 
 
 
+def next_player(row, col):
+    pass
 
 
+players_name = ["Almdrasa", "Mohali"]
+player = random.choice(players_name)
 
 font_family = 'Arial'
 font_size = 50
 
 root = tk.Tk()
 
-window_width = 500
-window_height = 500
+window_width = 600
+window_height = 600
 
 screen_width = root.winfo_screenwidth()
 screen_height = root.winfo_screenheight()
@@ -20,7 +25,7 @@ screen_coordinate_x = (screen_width // 2) - (window_width // 2)
 screen_coordinate_y = (screen_height // 2) - (window_height // 2)
 
 root.geometry(f'{window_width}x{window_height}+{screen_coordinate_x}+{screen_coordinate_y}')
-root.title('Tic_Tac_toe__Game from Almadrasa')
+root.title('Tic_Tac_Toe__Game from Almdrasa')
 root.rowconfigure(0, weight=0)
 root.rowconfigure(1, weight=8)
 root.columnconfigure(0, weight=1)
@@ -34,29 +39,34 @@ score_frame.columnconfigure(1, weight=1)
 player_score_label = tk.Label(score_frame, text='0', font=(font_family, font_size))
 player_score_label.grid(row=0, column=0, sticky='nesw')
 
-player_name_label = tk.Label(score_frame, text='Player Score', font=(font_family, 15))
+player_name_label = tk.Label(score_frame, text='Almdrasa Score', font=(font_family, 15))
 player_name_label.grid(row=1, column=0, sticky='nesw')
 
 
 computer_score_label = tk.Label(score_frame, text='0', font=(font_family, font_size))
 computer_score_label.grid(row=0, column=1, sticky='nesw')
 
-computer_name_label = tk.Label(score_frame, text='Computer Score', font=(font_family, 15))
+computer_name_label = tk.Label(score_frame, text='Mohali Score', font=(font_family, 15))
 computer_name_label.grid(row=1, column=1, sticky='nesw')
 
-message_label = tk.Label(root, text='Message', font=(font_family, 15))
+message_label = tk.Label(root, text=(player + ' turn'), font=(font_family, 15))
 message_label.grid(row=1, column=0, sticky='nesw')
 
-game_place_frame = tk.Frame(root, borderwidth=2, relief='groove', padx=5, pady=5)
+game_place_frame = tk.Frame(root, borderwidth=2, relief='groove') 
 game_place_frame.grid(row=2, column=0, sticky='nesw')
 
-grid = [[tk.Frame(game_place_frame, width=100, height=100, borderwidth=2, relief='groove') for _ in range(3)] for _ in range(3)]
+grid_btns = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0]
+]
+for row in range(3):
+    game_place_frame.rowconfigure(row, weight=1)
+    for col in range(3):
+        game_place_frame.columnconfigure(col, weight=1)
+        grid_btns[row][col] = tk.Button(game_place_frame, text="", font=(font_family, 50), width=4, height=1, command=lambda row=row, col=col: next_player(row, col))
+        grid_btns[row][col].grid(row=row, column=col, sticky="nsew", padx=5, pady=5)
 
-for i in range(3):
-    game_place_frame.rowconfigure(i, weight=1)
-    game_place_frame.columnconfigure(i, weight=1)
-    for j in range(3):
-        tk.Frame(game_place_frame, width=100, height=100, borderwidth=2, relief='groove').grid(row=i, column=j, sticky="nsew")
 
 control_place_frame = tk.Frame(root)
 control_place_frame.grid(row=3, column=0, sticky='s')
